a458a3d6b1cbf6593fb242b444636daed4580912
Abhishek Banthia <abhishekbanthia@me.com>
2015-09-22 11:56:43 -0400

    Initial Commit

---
+++ Sorts/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sorts
+//
+//  Created by Abhishek Banthia on 9/22/15.
+//  Copyright © 2015 Abhishek Banthia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
