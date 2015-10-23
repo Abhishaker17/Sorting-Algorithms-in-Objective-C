10fd08a13aac0371dabd45b5c3e60e667ef3fba6
Abhishek Banthia <abhishekbanthia@me.com>
2015-10-22 20:58:52 -0400

    Adding new solutions!

--- Sorts/ViewController.m
+++ Sorts/ViewController.m
@@ -17,11 +17,27 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    NSMutableArray *unsortedArray = [NSMutableArray arrayWithArray:@[@16, @5, @14, @3, @1]];
+//    NSMutableArray *unsortedArray = [NSMutableArray arrayWithArray:@[@16, @5, @14, @3, @1]];
     
 //    NSMutableArray *sortedArray = [NSMutableArray arrayWithArray:@[@14, @17, @33, @39, @102]];
     
-    [self mergeSortOnArray:unsortedArray];
+//    [self quickSort:unsortedArray withStartIndex:0 andEndIndex:unsortedArray.count-1];
+    
+//    [self hashTableMethods:[NSMutableArray array]];
+    
+//    [self rodCuttingProblem:[NSMutableDictionary dictionaryWithDictionary:@{@1 : @1, @2 : @3, @3 : @9, @4 : @13 }] AndInteger:4];
+    
+//    [self computingPowerOfNumber:2 toThePower:14];
+    
+//    NSLog(@"Hamming distance:%zd", [self compute:@"GAGCCTACTAACGGGAT" against:@"CATCGTAATGACGGCCT"]);
+    
+//    [self creatingRansomNoteFromMagazineString:@"Hello, is anybody in there?"];
+    
+    NSDate * now = [NSDate date];
+    NSDateFormatter *outputFormatter = [[NSDateFormatter alloc] init];
+    [outputFormatter setDateFormat:@"HH:mm:ss"];
+    NSString *newDateString = [outputFormatter stringFromDate:now];
+    NSLog(@"newDateString %@", newDateString);
     
     // Do any additional setup after loading the view, typically from a nib.
 }
@@ -46,6 +62,44 @@
    }];
     
     NSLog(@"Selection sort result:%@", array);
+}
+
+/*
+ Computing power of a number
+ 
+ */
+
+- (void)plantedMotifSearch:(NSString *)sequence withLength:(NSInteger)length andDifference:(NSInteger)difference
+{
+    
+}
+
+- (NSUInteger)compute:(NSString *)strand against:(NSString *)otherStrand {
+    unsigned long commonLength = MIN([strand length], [otherStrand length]);
+    int distance = 0;
+    
+    for (NSInteger i = 0; i < commonLength; i++) {
+        unichar nucleotide = [strand characterAtIndex:i];
+        unichar otherNucleotide = [otherStrand characterAtIndex:i];
+        
+        if (nucleotide != otherNucleotide) {
+            distance++;
+        }
+    }
+    
+    return distance;
+}
+
+- (void)computingPowerOfNumber:(NSInteger)number toThePower:(NSInteger)power
+{
+    NSInteger answer = number;
+    
+    for (NSInteger i = 1; i < power ; i++)
+    {
+        answer = answer * number;
+    }
+    
+    NSLog(@"Answer:%zd", answer);
 }
 
 /*
@@ -114,10 +168,15 @@
     NSLog(@"Insertion Sort Results:%@", array);
 }
 
+/*
+ 
+ Best and worst case time complexity is o(nlogn) 
+ 
+    Order of n for the merge function and the length of the recursion tree is logn
+ */
+
 - (void)mergeSortOnArray:(NSMutableArray *)array
 {
-   
-    
     //Array is sorted
     if (array.count < 2)
     {
@@ -143,6 +202,11 @@
     [self mergeSortOnArray:rightArray];
     [self mergeFunction:leftArray andRight:rightArray withFinalArray:array];
 }
+
+/*
+ 
+ The time complexity for this method will be O(n)
+ */
 
 - (void)mergeFunction:(NSMutableArray *) left andRight:(NSMutableArray *) right withFinalArray:(NSMutableArray *) finalArray
 {
@@ -179,6 +243,117 @@
     }
     
     NSLog(@"Final array:%@", finalArray);
+}
+
+/*
+ Best and Average case: O(nlogn) 
+ 
+ Best case is one where the subpartitions are almost of equal size i.e. Pivot divides the array into two.
+ 
+ Worst Case: O(n^2) --> Can be avoided by using randomized version of quicksort
+ 
+ Worst case will be for eg [1,2,3,4,5,6,7,8] where 8 is the pivot element matlab array is reverse sorted.
+ 
+ Merge Sort always divides in 2. But Quick sort does not which means one processor has to do more work than the other.
+ 
+ */
+
+- (void)quickSort:(NSMutableArray *)array withStartIndex:(NSInteger)startIndex andEndIndex:(NSInteger)endIndex
+{
+    if (startIndex < endIndex)
+    {
+        NSInteger pivotIndex = [self partitionArray:array andStartIndex:startIndex andEndIndex:endIndex];
+        
+        //The next two calls must not include the pivot element
+        
+        [self quickSort:array withStartIndex:startIndex andEndIndex:pivotIndex - 1];
+        
+        [self quickSort:array withStartIndex:pivotIndex + 1 andEndIndex:endIndex];
+    }
+    
+     NSLog(@"Quick sort implementation result:%@", array);
+}
+
+/*
+ 
+ Partitioning does not ensure stability; which means relative order won't be preserved all the time.
+ 
+ */
+
+- (NSInteger)partitionArray:(NSMutableArray *)array andStartIndex:(NSInteger)startIndex andEndIndex:(NSInteger)endIndex
+{
+    NSInteger partitionIndex = startIndex; //c1
+    
+    NSNumber *pivotValue = array[endIndex]; //c1
+    
+    for (NSInteger i = startIndex; i < endIndex; i++) //n*c3
+    {
+        if (array[i] < pivotValue)
+        {
+            [array exchangeObjectAtIndex:i withObjectAtIndex:partitionIndex];
+            partitionIndex++;
+        }
+    }
+    
+    [array exchangeObjectAtIndex:endIndex withObjectAtIndex:partitionIndex];
+    
+    return partitionIndex;
+}
+
+- (void)hashTableMethods:(NSMutableArray *)array
+{
+    NSHashTable *hashTable = [NSHashTable weakObjectsHashTable];
+    [hashTable addObject:@14];
+    [hashTable addObject:@17];
+    [hashTable addObject:@"Abhishek"];
+    NSLog(@"Hashtable 1:%@", hashTable);
+    NSLog(@"Does the hashtable contain Abhishek:%zd", [hashTable containsObject:@"Abhishek"]);
+    NSLog(@"Printing all objects of Hash Table:%zd", [hashTable allObjects]);
+    NSLog(@"Random Selection from Hash Table%zd", [hashTable anyObject]);
+    NSLog(@"Set representation:%@", [hashTable setRepresentation]);
+//    NSLog(@"Hash Table with weak reference objects:%@", );
+    
+}
+
+- (NSInteger)rodCuttingProblem:(NSMutableDictionary *)prices AndInteger:(NSInteger)integer
+{
+    if (integer == 0) {
+        return 0;
+    }
+    
+    NSInteger q = INFINITY;
+    
+    for (int i = 1; i <= integer ; i++) {
+        q = MAX(q, (NSInteger)[prices objectForKey:[NSNumber numberWithInt:i]] + [self rodCuttingProblem:prices AndInteger:integer - 1]);
+    }
+    
+//    NSLog(@"Return Value:%zd", q);
+    
+    return q;
+}
+
+- (NSString *)creatingRansomNoteFromMagazineString:(NSString *)magString
+{
+    NSMutableString *ransomString = [NSMutableString string];
+    
+    NSMutableArray *randomArray = [NSMutableArray array];
+    
+    [magString enumerateSubstringsInRange:NSMakeRange(0, magString.length) options:NSStringEnumerationByWords usingBlock:^(NSString * _Nullable substring, NSRange substringRange, NSRange enclosingRange, BOOL * _Nonnull stop) {
+        
+        [randomArray addObject:substring];
+        
+    }];
+    
+    NSInteger randomNumber = arc4random_uniform(randomArray.count);
+    
+    for (NSInteger i = 0; i <= randomNumber; i++)
+    {
+        [ransomString appendFormat:@"%@ ",randomArray[i]];
+    }
+    
+    NSLog(@"Ransom String:%@", ransomString);
+    
+    return ransomString;
 }
 
 @end
